50431e188a9420b528a5532c1ee83f2268023baa
Luca Fonstad <lucafonstad@Lucas-MacBook-Pro.local>
2020-11-24 16:32:30 -0500

    resized

--- src/resizeimages.py
+++ src/resizeimages.py
@@ -1,4 +1,5 @@
 from PIL import Image
+import numpy as np
 import os
 for r,d,f in os.walk("images"):
     for file in f:
@@ -9,4 +10,6 @@ for r,d,f in os.walk("images"):
                 image = image.resize(((int(float(x)/float(y)*256)), 256))
             else:
                 image = image.resize((256, (int(float(y)/float(x)*256))))
+            x, y = image.size
+            image = image.crop((int(float(x-224)/2.0), int(float(y-224)/2.0), x-int(float(x-224)/2.0), y-int(float(y-224)/2.0)))
             image.save(os.path.join(r, file))
